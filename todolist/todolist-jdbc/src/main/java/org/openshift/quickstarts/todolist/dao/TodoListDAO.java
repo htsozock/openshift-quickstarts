956a153bb015b67f4438132d7a9601ee1611645c
hilaire <htsozock@ains.com>
2016-03-14 13:22:07 -0400

    new changes -TodoDAO.java

--- todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/dao/TodoListDAO.java
+++ todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/dao/TodoListDAO.java
@@ -11,9 +11,5 @@ import java.util.List;
 public interface TodoListDAO {
 
     void save(TodoEntry entry);
-    List<TodoEntry> list(); // will return all the projects
-  //  void delete((String project_id);
-  //  void update(TodoEntry entry);
-   // public List<TodoEntry> read() // will return ONLY one project
-   // public List<TodoEntry> search() // will return one or many  projects based on some criterias 
+    List<TodoEntry> list(); 
 }
